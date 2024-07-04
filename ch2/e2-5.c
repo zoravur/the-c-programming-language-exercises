e479996669e21f3fae0dec81d05ae6a1b2c53c1b
Zoravur <z2singh@uwaterloo.ca>
2024-07-03 22:05:08 -0400

    more exercises

--- ch2/e2-5.c
+++ ch2/e2-5.c
@@ -4,6 +4,27 @@ where any character from the string s2 occurs, or -1 if s1 contains no character
 (The standard library function strpbrk does the same job but returns a pointer to the
 location.)
  */
+#include <unistd.h>
+#include <stdio.h>
 
+int any(const char *s1, const char *s2) {
+    int index[128] = {};
+    for (int i = 0; s2[i] != '\0'; ++i) index[s2[i]] = 1;
 
+    int j;
+    for (j = 0; s1[j] != '\0'; ++j) {
+        if (index[s1[j]]) return j;
+    }
+
+    return -1;
+}
+
+int main() {
+    char *s1, *s2;
+    size_t n1, n2;
+    s1=s2=NULL;
+    getline(&s1, &n1, stdin);
+    getline(&s2, &n2, stdin);
+    printf("%d\n", any(s1, s2));
+}
 
---
+++ ch2/e2-6.c
@@ -0,0 +1,19 @@
+
+
+
+
+
+unsigned setbits(unsigned x, unsigned p, unsigned n, unsigned y) {
+    // 010010011
+    // 000000100
+    // (x, 3, 3, y)
+    // 010100011
+    unsigned allones = ~0;
+    unsigned bits_to_assign = y & ~(allones << n);
+    unsigned 
+    
+    
+
+
+
+}
